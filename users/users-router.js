d1141e9ff42b90e0e00c463af5add7b96c491312
Devanee Reid <devaneereid@gmail.com>
2020-04-20 13:55:23 -0600

    added function to find user by id

--- users/users-router.js
+++ users/users-router.js
@@ -2,12 +2,31 @@ const router = require('express').Router();
 
 const Users = require('./users-model.js');
 
+// GET Users (all)
 router.get('/', (req, res) => {
     console.log('session', req.session);
-    
+
     Users.find()
         .then(users => {
             res.json(users);
+        })
+        .catch(error => {
+            console.log(error);
+            res.send(error);
+        });
+});
+
+// GET User by Id 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    
+    Users.findById(id)
+        .then(user => {
+            if (user) {
+                res.json(user);        
+            } else {
+                res.status(404).json({ message: 'Could not find user by that ID' });
+            }
         })
         .catch(error => {
             console.log(error);
