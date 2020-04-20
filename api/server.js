e7490e70e1e8924456612d0b3bba747253119b72
Devanee Reid <devaneereid@gmail.com>
2020-04-20 12:59:46 -0600

    migrations for database

--- api/server.js
+++ api/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const userRouter = require('../users/users-router.js');
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
     res.json({ api: 'API is up and running' });
